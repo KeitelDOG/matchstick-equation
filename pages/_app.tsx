95d0ac2562b71a089d4836c4f00ca0564329e50e
Keitel Jovin <keiteldog@gmail.com>
2023-05-19 15:54:15 -0400

    Add Env HOST variable to SEO tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,20 +5,20 @@ import Head from 'next/head';
 function MyApp({ Component, pageProps }: AppProps) {
   const title = 'Match Stick Equation Solver';
   const desc = 'Solve match stick Equation by moving, adding, removing some sticks.';
-  const imageUrl = '/match-stick-equation.png';
+  const imageUrl = `${process.env.NEXT_PUBLIC_HOST}/match-stick-equation.png`;
 
   return (
     <>
       <Head>
         <title>{title}</title>
-        <meta name="description" content={desc} key="description" />
+        <meta name="description" content={desc} />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="canonical" href={process.env.NEXT_PUBLIC_HOST} />
 
-        <meta property="og:title" content={title} />;
-        <meta property="og:description" content={desc} key="og:description" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={desc} />
         <meta property="og:image" content={imageUrl} />
-        <meta property="og:url" content="/" key="og:url" />;
-        <link rel="canonical" href="/" />;
+        <meta property="og:url" content={process.env.NEXT_PUBLIC_HOST} />
       </Head>
       <Component {...pageProps} />
     </>
